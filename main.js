1149505147a4a5e15c3d4ac784b6379bc7858c00
Gvantsa-Tabunidze <gvantsaletta9@gmail.com>
2025-03-07 20:01:18 +0400

    Home work 2

--- main.js
+++ main.js
@@ -1,49 +1,70 @@
 'use strict';
 
+// Home assignment 2
+
 // Task 1
 
-const checkNumbers = function (a, b) {
-  if (typeof a === 'number' && typeof b === 'number') {
-    return a + b;
-  } else {
-    return 'შეცდომა მატრიცაში';
-  }
-};
+const user = [
+  { name: 'Temo', age: 25 },
+  { name: 'Lasha', age: 21 },
+  { name: 'Ana', age: 28 },
+];
 
-const result = checkNumbers(2, 6);
-console.log(result);
+function getUserName() {
+  const minAge = Math.min(...user.map((item) => item.age));
+  return user.find((item) => item.age === minAge).name;
+}
+
+const youngestName = getUserName();
+console.log(youngestName);
 
 // Task 2
-function convertTemp(fahrenheit) {
-  if (typeof fahrenheit !== 'number') {
-    return false;
+// Function for the Player A
+function getRandomNum1() {
+  let randomNum1 = parseInt(Math.random() * 10 + 1);
+  return randomNum1;
+}
+let randomOutput1 = getRandomNum1();
+console.log(randomOutput1);
+
+// Function for the Player B
+function getRandomNum2() {
+  let randomNum2 = parseInt(Math.random() * 10) + 1;
+  return randomNum2;
+}
+let randomOutput2 = getRandomNum2();
+console.log(randomOutput2);
+
+function getWinner() {
+  if (randomOutput1 === 3 && randomOutput2 === 3) {
+    console.log('You both win');
   } else {
-    let celsius = ((fahrenheit - 32) * 5) / 9;
-    return celsius;
+    if (randomOutput1 === 3) {
+      console.log('A is a winner');
+    } else if (randomOutput2 === 3) {
+      console.log('B is a winner');
+    }
   }
 }
 
-console.log(convertTemp(45));
+getWinner();
 
 // Task 3
-function mathOperation(a, b, operation) {
-  if (typeof a !== 'number' && typeof b !== 'number') {
-    return false;
-  }
+const person = {
+  name: 'Sara',
+  age: 28,
+  occupation: 'Doctor',
+};
 
-  const validOperators = ['+', '-', '*', '/'];
-  if (validOperators.includes(operation)) {
-    if (operation === '/' && b === 0) {
-      return 'You can not divide by 0'
-    }
+let newPerson = {};
 
-    return eval(`${a}${operation}${b}`);
-  } else {
-    return 'Invalid operator';
-  }
+function createNewObj(item) {
+  newPerson[item] = person[item];
 }
 
-console.log(mathOperation(500, 200, '+'));
-console.log(mathOperation(500, 200, '-'));
-console.log(mathOperation(500, 200, '*'));
-console.log(mathOperation(500, 200, '/'));
+for (let key in person) {
+  createNewObj(key);
+}
+
+console.log(newPerson); //New object
+console.log(person); //Default object
